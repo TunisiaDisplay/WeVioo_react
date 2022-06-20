c01e5f69fd04f4b200e67ef9555ced3b5722140a
TunisiaDisplay <israr4dev@gmail.com>
2022-06-20 15:28:35 +0100

    install react router v6 & add routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Detail from './pages/Detail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="details" element={<Detail />} />
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
