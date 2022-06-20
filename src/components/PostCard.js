8bdbecd6465e06baffad3fc0eb0a1b0209121fda
TunisiaDisplay <israr4dev@gmail.com>
2022-06-20 12:43:16 +0100

    static card list

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,12 +1,36 @@
 import React from "react";
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const PostCard = ()=>{
+const PostCard = (props) => {
 
-    return(
-        <h2>
-            post card component
-        </h2>
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image={props.img || "/img/contemplative-reptile.jpg"}
+                alt="green iguana"
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                    species, ranging across all continents except Antarctica
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
     );
 }
 
--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -1,16 +1,44 @@
 import React from "react";
+import { Container } from "@mui/system";
+import Grid from '@mui/material/Grid';
+import Pagination from '@mui/material/Pagination';
+
 import PostCard from "../components/PostCard";
 
 
 const Listing = () => {
 
     return (
-        <div>
+        <Container maxWidth="lg">
             <h1>
                 listing page
             </h1>
-            <PostCard />
-        </div>
+            <Grid container spacing={2}>
+                <Grid item xs={8} md={5} lg={4}>
+                    <PostCard />
+                </Grid>
+                <Grid item xs={8} md={5} lg={4}>
+                    <PostCard img="/img/paella.jpg" />
+                </Grid>
+                <Grid item xs={8} md={5} lg={4}>
+                    <PostCard />
+                </Grid>
+                <Grid item xs={8} md={5} lg={4}>
+                    <PostCard img="/img/paella.jpg" />
+                </Grid>
+                <Grid item xs={8} md={5} lg={4}>
+                    <PostCard />
+                </Grid>
+                <Grid item xs={8} md={5} lg={4}>
+                    <PostCard img="/img/paella.jpg" />
+                </Grid>
+                <Grid item xs={12}>
+                    <Pagination count={10} color="primary" />
+                </Grid>
+            </Grid>
+
+
+        </Container>
 
     );
 }
