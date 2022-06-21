260e495eaf07b893e16a57413cfae92ac214476a
TunisiaDisplay <israr4dev@gmail.com>
2022-06-21 09:34:43 +0100

    customize post card & static posts list

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,37 +2,58 @@ import React from "react";
 
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import CardActionArea from '@mui/material/CardActionArea';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import { Link } from "react-router-dom";
 
 const PostCard = (props) => {
 
+    const post = props.post;
+
     return (
         <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-                component="img"
-                height="140"
-                image={props.img || "/img/contemplative-reptile.jpg"}
-                alt="green iguana"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all continents except Antarctica
-                </Typography>
-            </CardContent>
+            <CardActionArea LinkComponent={Link} to="/details">
+                <CardHeader
+                    avatar={
+                        <Avatar alt={post.owner.firstName} src={post.owner.picture} />
+                    }
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                    title={(post.owner.firstName + post.owner.lastName) || "First Name"}
+                    subheader={post.publishDate.substr(0, 10) || "PubDate"}
+                />
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={post.image || "/img/contemplative-reptile.jpg"}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        {post.text}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Link to="/details">
-                    <Button size="small">Learn More</Button>
-                </Link>
+                {
+                    post.tags.map(tag => {
+                        return(
+                            <Button LinkComponent={Link} to={"/details"} size="small">{tag}</Button>
+                        )
+                    })
+                }
+                
             </CardActions>
         </Card>
     );
--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -9,6 +9,129 @@ import MultipleSelect from "../components/MultipleSelect";
 import SimpleSelect from "../components/SimpleSelect";
 
 
+const postList = {
+    "data": [
+        {
+            "id": "60d21b4667d0d8992e610c85",
+            "image": "https://img.dummyapi.io/photo-1564694202779-bc908c327862.jpg",
+            "likes": 43,
+            "tags": [
+                "animal",
+                "dog",
+                "golden retriever"
+            ],
+            "text": "adult Labrador retriever",
+            "publishDate": "2020-05-24T14:53:17.598Z",
+            "owner": {
+                "id": "60d0fe4f5311236168a109ca",
+                "title": "ms",
+                "firstName": "Sara",
+                "lastName": "Andersen",
+                "picture": "https://randomuser.me/api/portraits/women/58.jpg"
+            }
+        },
+        {
+            "id": "60d21b4967d0d8992e610c90",
+            "image": "https://img.dummyapi.io/photo-1510414696678-2415ad8474aa.jpg",
+            "likes": 31,
+            "tags": [
+                "snow",
+                "ice",
+                "mountain"
+            ],
+            "text": "ice caves in the wild landscape photo of ice near ...",
+            "publishDate": "2020-05-24T07:44:17.738Z",
+            "owner": {
+                "id": "60d0fe4f5311236168a10a0b",
+                "title": "miss",
+                "firstName": "Margarita",
+                "lastName": "Vicente",
+                "picture": "https://randomuser.me/api/portraits/med/women/5.jpg"
+            }
+        },
+        {
+            "id": "60d21b8667d0d8992e610d3f",
+            "image": "https://img.dummyapi.io/photo-1515376721779-7db6951da88d.jpg",
+            "likes": 16,
+            "tags": [
+                "dog",
+                "pet",
+                "canine"
+            ],
+            "text": "@adventure.yuki frozen grass short-coated black do...",
+            "publishDate": "2020-05-24T05:44:55.297Z",
+            "owner": {
+                "id": "60d0fe4f5311236168a109ed",
+                "title": "miss",
+                "firstName": "Kayla",
+                "lastName": "Bredesen",
+                "picture": "https://randomuser.me/api/portraits/med/women/13.jpg"
+            }
+        },
+        {
+            "id": "60d21b3a67d0d8992e610c60",
+            "image": "https://img.dummyapi.io/photo-1581804928342-4e3405e39c91.jpg",
+            "likes": 7,
+            "tags": [
+                "canine",
+                "pet",
+                "mammal"
+            ],
+            "text": "Hiking with my dog in the woods. black labrador re...",
+            "publishDate": "2020-05-23T22:56:11.424Z",
+            "owner": {
+                "id": "60d0fe4f5311236168a109d5",
+                "title": "mrs",
+                "firstName": "Sibylle",
+                "lastName": "Leibold",
+                "picture": "https://randomuser.me/api/portraits/med/women/89.jpg"
+            }
+        },
+        {
+            "id": "60d21bf967d0d8992e610e9b",
+            "image": "https://img.dummyapi.io/photo-1574457547512-5b1646994eea.jpg",
+            "likes": 28,
+            "tags": [
+                "dog",
+                "human",
+                "animal"
+            ],
+            "text": "Two boys hug their dogs in a leaf pile in the fall...",
+            "publishDate": "2020-05-23T18:52:32.613Z",
+            "owner": {
+                "id": "60d0fe4f5311236168a109f7",
+                "title": "mrs",
+                "firstName": "Jolanda",
+                "lastName": "Lacroix",
+                "picture": "https://randomuser.me/api/portraits/med/women/32.jpg"
+            }
+        },
+        {
+            "id": "60d21b7d67d0d8992e610d25",
+            "image": "https://img.dummyapi.io/photo-1498534928137-473daa67f5c4.jpg",
+            "likes": 18,
+            "tags": [
+                "dog",
+                "animal",
+                "pet"
+            ],
+            "text": "Bone salt and pepper schnauzer puppy",
+            "publishDate": "2020-05-23T14:42:22.808Z",
+            "owner": {
+                "id": "60d0fe4f5311236168a109e4",
+                "title": "mr",
+                "firstName": "Pwry",
+                "lastName": "Shylyrd",
+                "picture": "https://randomuser.me/api/portraits/med/men/37.jpg"
+            }
+        },
+    ],
+    "total": 873,
+    "page": 0,
+    "limit": 6
+};
+
+
 const Listing = () => {
 
 
@@ -28,27 +151,20 @@ const Listing = () => {
                     />
                 </Grid>
 
-                <Grid item xs={8} md={5} lg={4}>
-                    <PostCard />
-                </Grid>
-                <Grid item xs={8} md={5} lg={4}>
-                    <PostCard img="/img/paella.jpg" />
-                </Grid>
-                <Grid item xs={8} md={5} lg={4}>
-                    <PostCard />
-                </Grid>
-                <Grid item xs={8} md={5} lg={4}>
-                    <PostCard img="/img/paella.jpg" />
-                </Grid>
-                <Grid item xs={8} md={5} lg={4}>
-                    <PostCard />
-                </Grid>
-                <Grid item xs={8} md={5} lg={4}>
-                    <PostCard img="/img/paella.jpg" />
-                </Grid>
+                {
+                    postList.data.map(post => {
+                        return (
+                            <Grid item xs={8} md={5} lg={4}>
+                                <PostCard post={post} />
+                            </Grid>
+                        );
+                    })
+                }
+
                 <Grid item xs={12}>
                     <Pagination count={10} color="primary" />
                 </Grid>
+
             </Grid>
 
 
