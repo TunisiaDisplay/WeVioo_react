d74ecec8648b1f71040939d695fb8417da74e422
TunisiaDisplay <israr4dev@gmail.com>
2022-06-22 11:18:11 +0100

    post details component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,11 +3,11 @@ import { useParams } from "react-router-dom";
 
 import { Container } from "@mui/system";
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
+
 import CircularProgress from '@mui/material/CircularProgress';
 
 import dummyApi from "../api/dummyApi";
+import PostDetails from "../components/PostDetails";
 
 
 const Detail = () => {
@@ -47,56 +47,8 @@ const Detail = () => {
                             <CircularProgress />
                         </Grid>
                     </Grid>
-
                     :
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <h1>
-                                detail page
-                            </h1>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Paper>
-                                <img src={postDetails.image} alt="" />
-                            </Paper>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <text>{postDetails.likes}</text>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <text>{postDetails.publishDate}</text>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <text>{postDetails.text}</text>
-                        </Grid>
-                        <Grid item xs={12}>
-                            {
-                                postDetails.tags.map(tag => {
-                                    return (
-                                        <text>{tag}</text>
-                                    )
-                                })
-                            }
-                        </Grid>
-                        <Grid item xs={12}>
-                            <img src={postDetails.owner.picture} alt="" />
-                            <text>{postDetails.owner.title}</text>
-                            <text>{postDetails.owner.firstName}</text>
-                            <text>{postDetails.owner.lastName}</text>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                id="outlined-multiline-static"
-                                label="Multiline"
-                                multiline
-                                rows={4}
-                                defaultValue="Default Value"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <text>List of comments</text>
-                        </Grid>
-                    </Grid>
+                    <PostDetails details={postDetails} />
             }
 
 
